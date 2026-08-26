24054cfd3660d42ae59d693911c6e6c0863b4c67
Anyang Ateny <anyangateny@outlook.com>
2026-08-26 22:50:01 +0930

    Add random QOL plugins

--- notes.md
+++ notes.md
@@ -2,3 +2,5 @@
 
 Tree sitter needs a version of rust running that command.
 It also needs clang strangely enough.
+
+Missing cursor highlight, I think it's an LSP thing.
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- { import = "plugins" },
+  { import = "plugins" },
   { import = "ui" },
   { import = "git" },
 }, {
