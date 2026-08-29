913a84c0fd54536da5fdec9f1cf84718200e8af1
Anyang Ateny <anyangateny@outlook.com>
2026-08-29 14:56:43 +0930

    Add guaranteed installation to mason

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,21 @@
 return {
-  "mason-org/mason.nvim",
-  opts = {},
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        -- Shell
+        "bash-language-server",
+        "shellcheck",
+        "shfmt",
+        -- Lua
+        "lua-language-server",
+        "stylua",
+      },
+    },
+  },
 }
