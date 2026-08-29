84c06119f0fb88e9176d2844cab38a14353d4eed
Anyang Ateny <anyangateny@outlook.com>
2026-08-29 14:57:46 +0930

    Format and add in-line diagnostics to lsp

--- nvim/lua/tooling/lsp.lua
+++ nvim/lua/tooling/lsp.lua
@@ -5,19 +5,61 @@ return {
     vim.lsp.enable("lua_ls")
     vim.lsp.enable("bashls")
 
-     vim.api.nvim_create_autocmd("LspAttach", {
+    vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
       callback = function(ev)
         local opts = { buffer = ev.buf }
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "LSP: Go to definition" }))
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, { desc = "LSP: Go to declaration" }))
+        vim.keymap.set(
+          "n",
+          "gd",
+          vim.lsp.buf.definition,
+          vim.tbl_extend("force", opts, { desc = "LSP: Go to definition" })
+        )
+        vim.keymap.set(
+          "n",
+          "gD",
+          vim.lsp.buf.declaration,
+          vim.tbl_extend("force", opts, { desc = "LSP: Go to declaration" })
+        )
         vim.keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "LSP: References" }))
-        vim.keymap.set("n", "gI", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "LSP: Go to implementation" }))
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "LSP: Hover documentation" }))
+        vim.keymap.set(
+          "n",
+          "gI",
+          vim.lsp.buf.implementation,
+          vim.tbl_extend("force", opts, { desc = "LSP: Go to implementation" })
+        )
+        vim.keymap.set(
+          "n",
+          "K",
+          vim.lsp.buf.hover,
+          vim.tbl_extend("force", opts, { desc = "LSP: Hover documentation" })
+        )
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "LSP: Rename" }))
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "LSP: Code action" }))
+        vim.keymap.set(
+          { "n", "v" },
+          "<leader>ca",
+          vim.lsp.buf.code_action,
+          vim.tbl_extend("force", opts, { desc = "LSP: Code action" })
+        )
       end,
+    })
+
+    vim.diagnostic.config({
+      severity_sort = true,
+      float = { source = "if_many" },
+      underline = { severity = vim.diagnostic.severity.ERROR },
+      signs = vim.g.have_nerd_font and {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.INFO] = "󰋽 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+      } or {},
+      virtual_text = {
+        source = "if_many",
+      },
     })
   end,
 }
