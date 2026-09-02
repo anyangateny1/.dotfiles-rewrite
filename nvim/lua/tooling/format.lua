3167bfc22ca6f206472b797f6d97eb69d471ea06
Anyang Ateny <anyangateny@outlook.com>
2026-09-02 20:53:38 +0930

    Add clang-format to cpp tooling

--- nvim/lua/tooling/format.lua
+++ nvim/lua/tooling/format.lua
@@ -1,3 +1,10 @@
+local style_files = { ".clang-format", "_clang-format", "clang-format.yaml" }
+
+local function find_clang_style(path)
+  return vim.fs.find(style_files, { path = path, upward = true })[1]
+    or (vim.fn.filereadable(vim.fs.normalize("~/.clang-format")) == 1 and vim.fs.normalize("~/.clang-format") or nil)
+end
+
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
@@ -7,7 +14,7 @@ return {
     {
       "<leader>f",
       function()
-        require("conform").format({ async = true })
+        require("conform").format({ async = true, lsp_format = "never" })
       end,
       mode = { "n", "v" },
       desc = "[F]ormat buffer",
@@ -17,20 +24,48 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
 
+      c = { "clang-format" },
+      cpp = { "clang-format" },
+
       sh = { "shfmt" },
 
       markdown = { "prettierd" },
 
       python = { "ruff" },
     },
+    formatters = {
+      ["clang-format"] = {
+        cwd = function(_, ctx)
+          local f = find_clang_style(ctx.filename)
+          return f and vim.fs.dirname(f) or nil
+        end,
+        prepend_args = function(_, ctx)
+          local f = find_clang_style(ctx.filename)
+          return f and { "-style=file:" .. f } or {}
+        end,
+      },
+    },
     -- Format on save can be disabled
     format_on_save = function(bufnr)
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 500, lsp_format = "fallback" }
+      return { timeout_ms = 500, lsp_format = "never" }
     end,
   },
+
+  vim.api.nvim_create_user_command("ClangFormatWhich", function()
+    local ft = vim.bo.filetype
+    if ft ~= "c" and ft ~= "cpp" then
+      vim.notify("ClangFormatWhich: not a C/C++ buffer (ft=" .. ft .. ")", vim.log.levels.INFO)
+      return
+    end
+    local f = find_clang_style(vim.api.nvim_buf_get_name(0))
+    vim.notify(
+      f and ("clang-format will use: " .. f) or "clang-format: no style file found",
+      f and vim.log.levels.INFO or vim.log.levels.WARN
+    )
+  end, { desc = "Show which clang-format config is used" }),
 
   vim.keymap.set("n", "<leader>cf", function()
     vim.b.disable_autoformat = not vim.b.disable_autoformat
--- nvim/lua/tooling/mason.lua
+++ nvim/lua/tooling/mason.lua
@@ -24,6 +24,7 @@ return {
         -- C/C++
         "clangd",
         "codelldb",
+        "clang-format",
         -- Python
         "pyright",
         "ruff",
